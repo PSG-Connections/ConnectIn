ddfb4fe7600e766e74f27d5198aa979a33c4d51c
iamvarun <varun@foxsense.io>
2023-02-25 18:09:25 +0530

    email validation using regex

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,36 +1,82 @@
 /* eslint-disable prettier/prettier */
-import React from 'react';
-import { TextInput, View,TouchableWithoutFeedback, TouchableOpacity, Keyboard,Image,Text} from 'react-native';
-import { Formik } from 'formik';
 
-function Login():JSX.Element {
+import {
+  Button,
+  Image,
+  Keyboard,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
+
+import {Formik} from 'formik';
+import {validEmail} from '../constants/regex';
+
+function Login(): JSX.Element {
+  const [emailErr, setEmailErr] = useState(false);
+  const [pwdError, setPwdError] = useState(false);
+
+  const validate = (email: any) => {
+    if (validEmail.test(email)) {
+      console.log('Valid Email');
+      setEmailErr(false);
+    } else {
+      setEmailErr(true);
+      console.log('Invalid Email');
+    }
+  };
   return (
-    <View className=" flex items-center justify-center  bg-white h-full w-full" >
-
+    <View className=" flex items-center justify-center  bg-white h-full w-full">
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <Image className="h-[250px] w-[350px] " source={require('../assets/loginScreen.png')} />
+        <Image
+          className="h-[250px] w-[350px] "
+          source={require('../assets/loginScreen.png')}
+        />
       </TouchableWithoutFeedback>
 
       <View className="shadow-2xl  shadow-black rounded-2xl bg-gray-100 w-[85%]  h-[45%] flex  items-center justify-center">
         <View className="h-auto pl-4  pb-5 pt-4 w-full text-black flex flex-col gap-2 ">
-            <Text className="text-4xl text-black font-bold">Sign In</Text>
-            <Text className="text-[16px] text-black">Stay Updated on your Professional World.</Text>
+          <Text className="text-4xl text-black font-bold">Sign In</Text>
+          <Text className="text-[16px] text-black">
+            Stay Updated on your Professional World.
+          </Text>
         </View>
 
         <Formik
-          initialValues={{email:'',password:''}}
-          onSubmit={(values)=>{
-            console.log('email ->',values.email);
-            console.log('password ->',values.password);
-          }}
-        >
-          {({handleChange,handleSubmit,values})=>(
+          initialValues={{email: '', password: ''}}
+          onSubmit={values => {
+            console.log('email ->', values.email);
+            console.log('password ->', values.password);
+          }}>
+          {({handleChange, handleSubmit, values}) => (
             <View className="w-[80%] flex flex-col gap-5 items-center justify-center">
-              <TextInput className="bg-gray-300 w-full rounded-full pl-4" placeholder="Email" keyboardType="email-address" onChangeText={handleChange('email')} value={values.email}/>
+              <TextInput
+                className="bg-gray-300 w-full rounded-full pl-4"
+                placeholder="Email"
+                keyboardType="email-address"
+                onChangeText={handleChange('email')}
+                value={values.email}
+              />
 
-              <TextInput className="bg-gray-300 w-full rounded-full pl-4" placeholder="Password" secureTextEntry={true} onChangeText={handleChange('password')} value={values.password} />
-              <Text className="font-bold text-[#1079D9] pl-4">Forgot Password?</Text>
-              <TouchableOpacity  onPress={handleSubmit} className="w-[70%]   " >
+              <TextInput
+                className="bg-gray-300 w-full rounded-full pl-4"
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={handleChange('password')}
+                value={values.password}
+              />
+              <Text className="font-bold text-[#1079D9] pl-4">
+                Forgot Password?
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  validate(values.email);
+                  handleSubmit();
+                }}
+                className="w-[70%]">
                 <Text className="bg-[#1079D9] rounded-3xl text-center pt-2 pb-2  text-white font-bold w-full ">
                   Sign In
                 </Text>
@@ -41,14 +87,12 @@ function Login():JSX.Element {
       </View>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View className="flex flex-row gap-2">
-          <Text  className="font-bold text-black pt-4">New to ....?</Text>
-          <Text  className="font-bold text-[#1079D9] pt-4">Join Us</Text>
+          <Text className="font-bold text-black pt-4">New to ....?</Text>
+          <Text className="font-bold text-[#1079D9] pt-4">Join Us</Text>
         </View>
-        </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
 
-
-
-export default Login;
+export default Login;
