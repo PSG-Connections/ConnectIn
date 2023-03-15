0e4f2ef61b27b7e4b0651ef1140c3ed2607e4211
iamvarun <varun@foxsense.io>
2023-03-15 17:47:53 +0530

    file uploading

--- src/apis/user.api.tsx
+++ src/apis/user.api.tsx
@@ -1,49 +1,70 @@
-
 // import { testLoginResponse } from '../constants/common.constant';
-import { HttpRequest } from './index';
-export async function RegisterUserAPI (data: any) {
+
+import {HttpRequest} from './index';
+
+export async function RegisterUserAPI(data: any) {
   const requestBody = {
     email: data?.email,
     password: data?.password,
     first_name: data?.firstName,
     last_name: data?.lastName,
-    phone_number: data?.phone
+    phone_number: data?.phone,
   };
   const axiosOptions = {
     url: '/api/user/register',
     method: 'POST',
-    data: requestBody
+    data: requestBody,
   };
   const response = await HttpRequest(axiosOptions);
   return response;
 }
 
-export async function GetUserByEmailAPI (data: any) {
+export async function GetUserByEmailAPI(data: any) {
   const axiosOptions = {
     url: '/api/user/'.concat(data.email),
     method: 'GET',
     headers: {
-      Authorization: 'Bearer '.concat()
-    }
-  };
-  const response = await HttpRequest(axiosOptions);
-  return response;
-};
-
-export async function CheckEmailExistsAPI (data: any) {
-  const axiosOptions = {
-    url: '/api/user/exist/'.concat(data.email),
-    method: 'GET'
+      Authorization: 'Bearer '.concat(),
+    },
   };
   const response = await HttpRequest(axiosOptions);
   return response;
 }
 
-export async function ResetPasswordAPI (data: any) {
+export async function CheckEmailExistsAPI(data: any) {
+  const axiosOptions = {
+    url: '/api/user/exist/'.concat(data.email),
+    method: 'GET',
+  };
+  const response = await HttpRequest(axiosOptions);
+  return response;
+}
+
+export async function ResetPasswordAPI(data: any) {
   const axiosOptions = {
     url: '/api/user/reset-pwd',
     method: 'PUT',
-    data
+    data,
+  };
+  const response = await HttpRequest(axiosOptions);
+  return response;
+}
+
+export async function UploadAvatarAPI(data: any) {
+  const requestBody = {
+    image: data,
+    email: '19pt33@psgtech.ac.in',
+    image_name: '4.png',
+    image_type: 'png',
+  };
+  const axiosOptions = {
+    url: '/api/user/profile-image',
+    method: 'POST',
+    data: requestBody,
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      // Accept: 'multipart/form-data',
+    },
   };
   const response = await HttpRequest(axiosOptions);
   return response;
--- src/screens/profile.screen.tsx
+++ src/screens/profile.screen.tsx
@@ -1,9 +1,50 @@
-import React, { useContext } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { AuthContext } from '../contexts/auth.context';
-import { clearEncryptedItemByKey } from '../helpers/utils';
+import {Button, Text, TouchableOpacity, View} from 'react-native';
+import React, {useContext} from 'react';
 
-export default function Profile (): JSX.Element {
+import {AuthContext} from '../contexts/auth.context';
+import {Blob} from 'buffer';
+import {UploadAvatarAPI} from '../apis/user.api';
+import {clearEncryptedItemByKey} from '../helpers/utils';
+import {launchImageLibrary} from 'react-native-image-picker';
+
+const options: any = {
+  title: 'Select Image',
+  type: 'library',
+  options: {
+    selectionLimit: 0,
+    mediaType: 'photo',
+    includeBase64: false,
+  },
+};
+export default function Profile(): JSX.Element {
+  const [avatar, setAvatar] = React.useState<any>();
+
+  const openGallery = async () => {
+    const images = await launchImageLibrary(options);
+    console.log('ASSETS', images?.assets?.[0]);
+    const formData = new FormData();
+    let uri = images.assets?.[0].uri;
+    let type = images.assets?.[0].type;
+    let name = images.assets?.[0].fileName;
+    formData.append('file', {
+      uri,
+      type,
+      name,
+    });
+    // formData.append(
+    //   'file',
+    //   images?.assets[0],
+    //   // {
+    //   //   uri: images.assets?.[0].uri as string,
+    //   //   type: images.assets?.[0].type || '',
+    //   //   name: images.assets?.[0].fileName || '',
+    //   // },
+    // );
+    // const response = await UploadAvatarAPI(formData);
+    // console.log('res', response);
+    // let responseJson = await response.json();
+    // console.log('resJson', responseJson);
+  };
   const authContext = useContext(AuthContext);
   const handleOnPressLogOut = async () => {
     try {
@@ -13,19 +54,20 @@ export default function Profile (): JSX.Element {
     } catch (error) {
       console.log(error);
     }
-    authContext.dispatch({ type: 'SIGNED_OUT' });
+    authContext.dispatch({type: 'SIGNED_OUT'});
   };
   return (
     <View className="h-screen bg-amber-100 items-center justify-center">
-        <Text className="text-black">Profile here ...............</Text>
-        <TouchableOpacity onPress={() => {
+      <Text className="text-black">Profile here ...............</Text>
+      <Button title="Upload" onPress={openGallery} />
+      <TouchableOpacity
+        onPress={() => {
           void (async () => {
             await handleOnPressLogOut();
           })();
         }}>
-          <Text className="font-bold text-[#1079D9] pt-4">Log Out</Text>
-          </TouchableOpacity>
-  </View>
-
+        <Text className="font-bold text-[#1079D9] pt-4">Log Out</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
